330d03e14be4b8c4f9ae0667c8d7f577c6f9b5e3
Peter <omgpieisawesome@hotmail.com>
2020-09-20 15:27:50 +0100

    Update discordRPC.js

--- public/discordRPC.js
+++ public/discordRPC.js
@@ -8,11 +8,11 @@ exports.initRPC = () => {
   client = new Client({ transport: 'ipc' });
 
   activity = {
-    details: 'Playing GDLauncher',
+    details: 'Playing VNLauncher',
     state: 'Idle',
     startTimestamp: Math.floor(Date.now() / 1000),
     largeImageKey: 'default_big',
-    largeImageText: 'GDLauncher - A Custom Minecraft Launcher',
+    largeImageText: 'VNLauncher - Based on GDLauncher',
     instance: false
   };
 
